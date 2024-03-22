a27fe7fad4f73699210be50ad4b02e8f7969a424
TomD101 <t.dowden101@gmail.com>
2024-03-22 16:50:58 +0000

    Almost done but verify not working

---
+++ holiday.py
@@ -0,0 +1,56 @@
+# Holiday cost calculator
+
+# Inputs for city_flight (create options), num_nights, rental_days (days hiring a car)
+
+#city = ["London", "Barcelona", "Milan", "Prague", "Copenhagen"]
+# Library of flight prices based on city
+
+
+# Functions for hotel, plane and car rental costs
+def hotel_cost (num_nights):
+    hotel_price = 30.45
+    return hotel_price * num_nights
+# dictionary for cities and costs
+def plane_cost(city_flight):
+    flight_costs = {
+        "London": 25, 
+        "Barcelona": 45, 
+        "Milan": 60, 
+        "Prague": 55,
+        "Copenhagen": 75,
+    }
+    #tried to get if else to validate city but couldn't get it to work
+    #return flight_costs.get(city_flight)
+    #if city_flight in flight_costs.keys:
+    return flight_costs[city_flight]
+    #else:
+    #print("The destination entered is not available. Please choose again.")
+
+
+# car_rental function based on days * daily cost
+def car_rental (rental_days):
+    car_cost = 15.50
+    return car_cost * rental_days
+
+# Input questions
+city_flight = input ("Where do you want to fly to? Your choices are London, Barcelona, Milan, Prague or Copenhahen : ")
+num_nights = int(input("How many nights is your trip? : "))
+rental_days = int(input("Hom many days do you need a rental car for? : "))
+
+# Function for holiday total cost
+def holiday_cost(num_nights, city_flight, rental_days):
+
+# Returns - The total cost of the holiday (hotel + flight + car rental)
+    hotel_price = hotel_cost(num_nights)
+    plane_price = plane_cost(city_flight)
+    car_price = car_rental(rental_days)
+    total_cost = hotel_price + plane_price + car_price
+    return total_cost
+
+# Printouts of trip details and total costs
+print(f"Your trip will cost £{plane_cost(city_flight)} for the flights,")
+print(f"£{hotel_cost(num_nights)} for the hotel,")
+print(f"and £{(car_rental(rental_days))} for the car rental.")
+print(f"Total cost for your trip is £{holiday_cost(num_nights, city_flight, rental_days)}")
+
+
