d4697e3dccc9fc7c67870cb2a90db496b92f3f8b
TomD101 <t.dowden101@gmail.com>
2024-03-24 16:54:44 +0000

    Bugs remaining

--- holiday.py
+++ holiday.py
@@ -8,17 +8,20 @@
 
 # Functions for hotel, plane and car rental costs
 def hotel_cost (num_nights):
-    hotel_price = 30.45
-    return hotel_price * num_nights
+    hotel_price = 130.60
+    return (hotel_price) * (num_nights)
 # dictionary for cities and costs
-def plane_cost(city_flight):
-    flight_costs = {
-        "London": 25, 
-        "Barcelona": 45, 
-        "Milan": 60, 
-        "Prague": 55,
-        "Copenhagen": 75,
+
+flight_costs = {
+        "Brussels": 65.50, 
+        "Barcelona": 70.25, 
+        "Milan": 70.79, 
+        "Prague": 55.05,
+        "Copenhagen": 79.99,
     }
+
+def plane_cost(city_flight):
+    
     #tried to get if else to validate city but couldn't get it to work
     #return flight_costs.get(city_flight)
     #if city_flight in flight_costs.keys:
@@ -29,17 +32,20 @@ def plane_cost(city_flight):
 
 # car_rental function based on days * daily cost
 def car_rental (rental_days):
-    car_cost = 15.50
+    car_cost = 35.75
     return car_cost * rental_days
 
 # Input questions
-city_flight = input ("Where do you want to fly to? Your choices are London, Barcelona, Milan, Prague or Copenhahen : ")
-num_nights = int(input("How many nights is your trip? : "))
-rental_days = int(input("Hom many days do you need a rental car for? : "))
-
+city_flight = input (f"Where do you want to fly to? Your options are: {' / '.join(flight_costs.keys())}: ")
+#if city_flight in flight_costs.keys():
+if city_flight in flight_costs.keys():
+    print ("Sorry, this destination is not available, please start again. ")
+else:
+    num_nights = int(input("How many nights is your trip? : "))
+    rental_days = int(input("Hom many days do you need a rental car for? : "))
+ 
 # Function for holiday total cost
 def holiday_cost(num_nights, city_flight, rental_days):
-
 # Returns - The total cost of the holiday (hotel + flight + car rental)
     hotel_price = hotel_cost(num_nights)
     plane_price = plane_cost(city_flight)
@@ -49,8 +55,8 @@ def holiday_cost(num_nights, city_flight, rental_days):
 
 # Printouts of trip details and total costs
 print(f"Your trip will cost £{plane_cost(city_flight)} for the flights,")
-print(f"£{hotel_cost(num_nights)} for the hotel,")
-print(f"and £{(car_rental(rental_days))} for the car rental.")
+print(f"£{hotel_cost(num_nights)} for {num_nights} night(s) in a hotel,")
+print(f"and £{(car_rental(rental_days))} for {rental_days} day(s) car rental.")
 print(f"Total cost for your trip is £{holiday_cost(num_nights, city_flight, rental_days)}")
 
 
